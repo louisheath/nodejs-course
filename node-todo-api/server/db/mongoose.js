c1a4fe54b328ffe85064aaed4038957768e19ee9
Louis Heath <lheath1998@gmail.com>
2019-01-05 21:34:41 +0000

    mlab env vars

--- node-todo-api/server/db/mongoose.js
+++ node-todo-api/server/db/mongoose.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const {mlab} = require('./../../keys');
-
-const local = 'mongodb://localhost:27017/TodoApp';
-const mlab = `mongodb://${mlab.username}:${mlab.password}@ds149754.mlab.com:49754/todo-app`;
+const localURI = 'mongodb://localhost:27017/TodoApp';
+const mlabURI = `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASS}@ds149754.mlab.com:49754/todo-app`;
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mlab);
+mongoose.connect(mlabURI);
 
 module.exports = {mongoose};
